61e0e99281394e244de5b837ae7800574297378b
Richard Hodgson <rightaboutnow@gmail.com>
2012-10-15 00:37:13 +0100

    Set file permissions correctly

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ VERSION=0.0.3
 
 mkdir -p /tmp/gin
 curl -L "https://github.com/richardhodgson/gin/tarball/v$VERSION" | tar -xz -C /tmp/gin
-cp /tmp/gin/richardhodgson-gin-*/gin /usr/local/bin
+cp /tmp/gin/richardhodgson-gin-*/gin /usr/local/bin/gin
+chmod +x /usr/local/bin/gin
 rm -fr /tmp/gin/richardhodgson-gin-*
 
-echo -e "Gin (version $VERSION) installed."
+echo "Gin (version $VERSION) installed."
