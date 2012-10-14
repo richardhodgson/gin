973c1108ed62dea28f21f6b357ad2cce6904c189
Richard Hodgson <rightaboutnow@gmail.com>
2012-10-14 20:44:43 +0100

    Added an install script to download the tag

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VERSION=0.0.0
+
+mkdir -p /tmp/gin
+curl -L "https://github.com/richardhodgson/gin/tarball/v$VERSION" | tar -xz -C /tmp/gin
+cp /tmp/gin/richardhodgson-gin-*/gin /usr/local/bin
+rm -fr /tmp/gin/richardhodgson-gin-*
+
+echo -e "Gin (version $VERSION) installed."
